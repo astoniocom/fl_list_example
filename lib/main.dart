efa49fcb6a3899daae41579ae1c0eefb9de8e839
Viktar Katlianik <7532425@gmail.com>
2022-02-13 23:33:12 +0200

    Basic project created

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("List Demo")),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(title: Text("Item $index"));
+        },
+      ),
+    );
+  }
+}
